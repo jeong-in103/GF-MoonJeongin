b1c8d54250c5bc485e1afd0eb07dc7ccba16334a
20201312 문정인 <83389445+jeong-in103@users.noreply.github.com>
2021-09-10 02:27:30 +0000

    2. HelloSDL

--- main.cpp
+++ main.cpp
@@ -1,15 +1,59 @@
-#include <stdio.h>
+#include <SDL.h>
+
+SDL_Window* g_pWindow = 0; // 윈도우 관련 정보 저장
+SDL_Renderer* g_pRenderer = 0; // 랜더링 상태를 포함하는 구조체
+bool g_bRunning = false;
+
+bool init(const char* title, int xpos, int ypos,
+          int height, int width, int flags) //초기화
+{
+  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  {
+    g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);// 윈도우 관련 정보 생성
+
+      if(g_pWindow != 0)
+      {
+        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);// 랜더링 상태 생성
+      }
+  }
+  else
+  {
+    return false;
+  }
+
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255); // 그리기 동작에 사용할 색 지정
+
+  return true;
+}
+
+void render()
+{
+  SDL_RenderClear(g_pRenderer); // 지정한 색으로 rederer 지우기
+  SDL_RenderPresent(g_pRenderer); // 백버퍼를 화면에 표시
+}
 
 int main(int argc, char* args[])
 {
-  int a = 0;
-  int b = 0;
+  if(init("Breaking Up helloSDL",
+          SDL_WINDOWPOS_CENTERED,
+          SDL_WINDOWPOS_CENTERED,
+          640, 480,
+          SDL_WINDOW_SHOWN))
+  {
+    g_bRunning = true;
+  }
+  else
+  {
+    return 1; //something's wrong
+  }
 
-  scanf("%d %d",&a, &b);
+  while(g_bRunning)
+  {
+    // handle input - update - render
+    render();
+  }
 
-  printf("%d\n", a + b);
-
-  printf("%d\n", a - b);
+  SDL_Quit();
 
   return 0;
 }
