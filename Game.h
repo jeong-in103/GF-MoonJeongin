c66ade6a19961c640dbf82400808c362d8a4a689
20201312 문정인 <83389445+jeong-in103@users.noreply.github.com>
2021-09-17 03:10:56 +0000

    SDL 기본 그리기 실습

---
+++ Game.h
@@ -0,0 +1,27 @@
+#ifndef __Game__
+#define __Game__
+
+#include "SDL.h"
+
+class Game{
+public:
+  Game() {}
+  ~Game() {}
+
+  bool init(const char *title, int xpos, int ypos, int width, int height, int flags);
+  void render();
+  void update();
+  bool running();
+  void handleEvents();
+  void clean();
+
+private:
+  SDL_Window* m_pWindow;
+  SDL_Renderer* m_pRenderer;
+  SDL_Texture* m_pTexture;
+  SDL_Rect m_sourceRectangle;
+  SDL_Rect m_destinationRectangle;
+  bool m_bRunning;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,66 +1,19 @@
-#include <SDL.h>
-#include <cstdlib>
+#include "Game.h"
+#include <stdio.h>
 
-SDL_Window* g_pWindow = 0; // 윈도우 관련 정보 저장
-SDL_Renderer* g_pRenderer = 0; // 랜더링 상태를 포함하는 구조체
-bool g_bRunning = false;
+Game* g_game = 0;
 
-bool init(const char* title, int xpos, int ypos,
-          int height, int width, int flags) //초기화
+int main(int argc, char* argv[])
 {
-  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  g_game = new Game();
+  g_game->init("Game Class", 100, 100, 640, 480, 0);
+
+  while(g_game->running())
   {
-    g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);// 윈도우 관련 정보 생성
-
-      if(g_pWindow != 0)
-      {
-        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);// 랜더링 상태 생성
-      }
+    g_game->handleEvents();
+    g_game->update();
+    g_game->render();
   }
-  else
-  {
-    return false;
-  }
-
-  return true;
-}
-
-void changeColor() // 렌더링 색을 랜덤으로 전환한다.
-{
-  SDL_SetRenderDrawColor(g_pRenderer, rand() % 255, rand() % 255, rand() % 255, 255);
-  SDL_Delay(1000);
-}
-
-void render()
-{
-  SDL_RenderClear(g_pRenderer); // 지정한 색으로 rederer 지우기
-  SDL_RenderPresent(g_pRenderer); // 백버퍼를 화면에 표시
-}
-
-int main(int argc, char* args[])
-{
-  if(init("partySDL",
-          SDL_WINDOWPOS_CENTERED,
-          SDL_WINDOWPOS_CENTERED,
-          640, 480,
-          SDL_WINDOW_SHOWN))
-  {
-    g_bRunning = true;
-  }
-  else
-  {
-    return 1; //something's wrong
-  }
-
-  while(g_bRunning)
-  {
-    // handle input - update - render
-    changeColor();
-
-    render();
-  }
-
-  SDL_Quit();
-
+  g_game->clean();
   return 0;
 }
